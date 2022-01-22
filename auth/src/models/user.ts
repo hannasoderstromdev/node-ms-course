ed4e5dfa32104d7ca075acff8a79df89946383d7
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-22 16:01:38 +0100

    implement password util

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "./../services/password";
 
 interface UserAttrs {
   email: string;
@@ -24,6 +25,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+  done();
 });
 
 // Add custom method to allow for typescript types
