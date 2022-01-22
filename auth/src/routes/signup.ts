d5d1b709113dc2940e8b1cb6f25afb10863ea254
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-22 15:30:13 +0100

    implement user model

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-errors";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4 })
       .withMessage("Enter a password of at least 4 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,11 +23,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
+    const existingUser = await User.findOne({ email });
 
-    // throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log("Email already in use");
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
