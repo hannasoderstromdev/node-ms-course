6f7adc831674f4ceb6bd09dc803785dd5e3ce582
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-22 15:30:27 +0100

    add mongoose and async errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -19,13 +21,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.get("*", () => {
+app.all("*", () => {
   throw new NotFoundError();
 });
 
 // Add error handler
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000...");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+  });
+};
+
+start();
