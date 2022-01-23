04213d0a01b2bed3003269243d366543a95764a0
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-23 14:25:51 +0100

    add errors and login copy

--- auth/src/copy.ts
+++ auth/src/copy.ts
@@ -6,4 +6,14 @@ export default {
     "password-required": "Please enter a password",
     "password-min": "Please enter a password of at least 4 characters",
   },
+  login: {
+    failed: "Login failed",
+  },
+  errors: {
+    "not-authorized": "Not authorized",
+    "db-connect-error": "Error connecting to database",
+    "not-found": "Not found",
+    "validation-error": "Validation error",
+    "unknown-error": "Something went wrong",
+  },
 };
