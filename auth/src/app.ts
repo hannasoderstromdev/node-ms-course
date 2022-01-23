d91cefe4220dfa3b5c3c47aa9953be2ff6e45492
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-23 14:57:13 +0100

    refactor: app config

---
+++ auth/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true); // Allow ingress to be proxy
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // not needed for JWT
+    secure: true, // adds SSL
+  })
+);
+
+// Add routing
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+// Add error handler
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,6 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); // Allow ingress to be proxy
-
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // not needed for JWT
-    secure: true, // adds SSL
-  })
-);
-
-// Add routing
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-
-// Add error handler
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_SECRET) {
