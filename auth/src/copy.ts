8a7a9853b0a553cc2dc219033f1f512fe2c48b75
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-23 13:25:48 +0100

    add copy

---
+++ auth/src/copy.ts
@@ -0,0 +1,9 @@
+export default {
+  validation: {
+    "email-required": "Please enter an email",
+    "email-invalid": "Please enter an valid email",
+    "email-in-use": "This email is already in use",
+    "password-required": "Please enter a password",
+    "password-min": "Please enter a password of at least 4 characters",
+  },
+};
