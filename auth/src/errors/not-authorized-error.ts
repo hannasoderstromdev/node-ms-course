8d84b6c36044853f106013a66e62018a2e26ca46
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-23 14:30:35 +0100

    add missing constructor

--- auth/src/errors/not-authorized-error.ts
+++ auth/src/errors/not-authorized-error.ts
@@ -4,6 +4,12 @@ import copy from "../copy";
 export class NotAuthorizedError extends CustomError {
   statusCode = 401;
 
+  constructor() {
+    super(copy.errors["not-authorized"]);
+
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
   serializeErrors() {
     return [{ message: copy.errors["not-authorized"] }];
   }
