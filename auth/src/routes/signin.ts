06844b10f7742a5959ec20f3a4136dcceeae61ab
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-23 14:28:24 +0100

    handle signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,59 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { Password } from "../services/password";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import copy from "../copy";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("signin");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage(copy.validation["email-required"]),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage(copy.validation["password-required"]),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError(copy.login.failed);
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError(copy.login.failed);
+    }
+
+    // generate JWT
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET! // ! tells TS to ignore this
+    );
+
+    // store in session
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
