0460a613ca6a34b9ae06346d2b63d85aef3d291e
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-29 12:34:35 +0100

    feat: show if logged in

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,24 @@
-const LandingPage = () => <h1>Landing Page</h1>;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => (
+  <div className="container">
+    <h1>Landing Page</h1>
+    <div>
+      {currentUser ? <p>You are signed in.</p> : <p>You are not signed in.</p>}
+    </div>
+  </div>
+);
+
+LandingPage.getInitialProps = async (context) => {
+  // On initial server-side render
+  try {
+    const client = buildClient(context);
+    const { data } = await client.get("/api/users/currentuser");
+    return { currentUser: data.currentUser };
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
 
 export default LandingPage;
