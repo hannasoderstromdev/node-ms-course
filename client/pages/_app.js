21e70f625c47b7894f72a699eb9dd1d975ba3496
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-29 13:25:13 +0100

    feat: implement Header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,34 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+import Header from "../components/Header";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+      <footer>(c) 2022 by Hanna Söderström</footer>
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  try {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get("/api/users/currentuser");
+
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+      pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return { currentUser: data.currentUser, pageProps };
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
+
+export default AppComponent;
