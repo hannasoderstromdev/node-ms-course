434a8a393992121d11957f5e80b12b2a96097611
Hanna Söderström <gothbarbie84@gmail.com>
2022-01-29 09:58:51 +0100

    test: use getAuthCookie

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,32 @@
+import { currentUser } from "./../../middlewares/current-user";
+import request from "supertest";
+
+import { getAuthCookie } from "./../../test/getAuthCookie";
+import { app } from "../../app";
+
+it("returns current user", async () => {
+  const cookie = await getAuthCookie();
+
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body).toEqual({
+    currentUser: {
+      email: "test@test.com",
+      iat: expect.any(Number),
+      id: expect.any(String),
+    },
+  });
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
