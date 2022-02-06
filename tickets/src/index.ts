0d717bce4aa294d06abb4ad265ba474d03d134fa
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-06 13:32:36 +0100

    fix: error messages

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,15 +5,15 @@ import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error("JWT_SECRET must be defined");
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
 
   if (!process.env.NATS_URL) {
-    throw new Error("JWT_SECRET must be defined");
+    throw new Error("NATS_URL must be defined");
   }
 
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error("JWT_SECRET must be defined");
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   if (!process.env.JWT_SECRET) {
