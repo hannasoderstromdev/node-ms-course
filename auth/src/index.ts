a081857524c3954e0e620394646ebe012f9e2e72
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-06 13:31:11 +0100

    feat: add console to show MongoDB connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,6 +13,7 @@ const start = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongodDB");
   } catch (err) {
     console.error(err);
   }
