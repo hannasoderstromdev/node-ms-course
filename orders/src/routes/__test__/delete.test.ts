277a326652d1a43128c6f6d18478f0b60fa30150
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-12 09:49:35 +0100

    feat: add delete route and test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,67 @@
+import { getAuthCookie } from "./../../test/getAuthCookie";
+import request from "supertest";
+
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it("returns 401 for unauthorized user", async () => {
+  await request(app)
+    .delete("/api/orders/61ffaffdd6bf0986ef007af3")
+    .send()
+    .expect(401);
+});
+
+it("returns 401 for wrong user", async () => {
+  const ticket = Ticket.build({ title: "Title", price: 20 });
+  await ticket.save();
+
+  const userOne = getAuthCookie();
+  const userTwo = getAuthCookie();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", userTwo)
+    .send()
+    .expect(401);
+});
+
+it("returns 404 on order not found", async () => {
+  const user = getAuthCookie();
+  await request(app)
+    .delete("/api/orders/61ffaffdd6bf0986ef007af3")
+    .set("Cookie", user)
+    .send()
+    .expect(404);
+});
+
+it("returns 204 on order cancelled", async () => {
+  const ticket = Ticket.build({ title: "Concert", price: 20 });
+  await ticket.save();
+
+  const user = getAuthCookie();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  const orderFound = await Order.findById(order.id);
+
+  expect(orderFound?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo("emits an order cancelled event");
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,36 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@hs-tickets/common";
+
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const orderFound = await Order.findById(orderId);
+
+    if (!orderFound) {
+      throw new NotFoundError();
+    }
+
+    if (orderFound.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    orderFound.status = OrderStatus.Cancelled;
+
+    await orderFound.save();
+
+    res.status(204).send({});
+  }
+);
 
 export { router as deleteOrderRouter };
